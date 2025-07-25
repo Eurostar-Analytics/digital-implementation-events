fd43b1aaf405e7194bebfdfb5f9408bd7ac00005
Kam Rafique <kamran.rafique@eurostar.com>
2025-07-25 16:15:43 +0100

    Update expectedObjectsViews.js

--- expectedObjectsViews.js
+++ expectedObjectsViews.js
@@ -36,7 +36,7 @@ window.expectedTealiumViewObjects = [
     sPaxYouth: /^\d+$/,
     sPaxInfants: /^\d+$/,
     sPaxSenior: /^\d+$/,
-    sPaxCompanion: /^\d+$/,
+    sPaxCompanion: /^(\d+)?$/,
     sPaxTotal: /^\d+$/,
     basket_value: /^\d+$/,
     basket_value_inpoints: /^\d+$/,
